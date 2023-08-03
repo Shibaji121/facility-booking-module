b326f97085c5a890440610e17562b664ccb59e93
shiba_1234 <shibajisahu121@gmail.com>
2023-08-03 17:05:27 +0530

    State data validation completed

--- src/components/Availability.js
+++ src/components/Availability.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import facilitiesAvailable from "../config/AvailableFacility";
 
 function Availability() {
+  const [strTime, setStrTime] = useState(0);
+  const [extTime, setExtTime] = useState(24);
+
   const { facilityName } = useParams();
   const slotAvailibilty = facilitiesAvailable.filter((facility) => {
     return facility.name === facilityName;
   });
+
+  const handleBooking = () => {
+    if (strTime < 0 || extTime < 0) {
+      alert("Start and Exit Time Should be greater than 0");
+      return;
+    } else if (strTime === "" || extTime === "") {
+      alert("Please Enter the Correct value in Start and Exit Time field");
+      return;
+    } else if (strTime > 24 || extTime > 25) {
+      alert("Start time and Exit time should be 00 to 24");
+      return;
+    } else if (strTime >= extTime) {
+      alert("Start Time Should be less than Exit Time");
+      return;
+    }
+    console.log("Validated");
+  };
 
   return (
     <div>
@@ -27,10 +47,20 @@ function Availability() {
         </div>
         <div className="details">
           <label>Enter The Start Time in 24hr format</label>
-          <input type="number" />
+          <input
+            type="number"
+            value={strTime}
+            onChange={(e) => setStrTime(e.target.value)}
+          />
           <label>Enter The Exit Time in 24hr format</label>
-          <input type="number" />
-          <button type="submit">Confirm Booking</button>
+          <input
+            type="number"
+            value={extTime}
+            onChange={(e) => setExtTime(e.target.value)}
+          />
+          <button type="submit" onClick={handleBooking}>
+            Confirm Booking
+          </button>
         </div>
       </div>
     </div>
