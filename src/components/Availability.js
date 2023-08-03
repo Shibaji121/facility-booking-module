a862f8995786b6440b19c61a83ae4873c4517a0c
shiba_1234 <shibajisahu121@gmail.com>
2023-08-03 14:44:11 +0530

    slot time and price fetched correctly

--- src/components/Availability.js
+++ src/components/Availability.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import facilitiesAvailable from "../config/AvailableFacility";
 
 function Availability() {
+  const { facilityName } = useParams();
+  const slotAvailibilty = facilitiesAvailable.filter((facility) => {
+    return facility.name === facilityName;
+  });
+
   return (
     <div>
-      <h1>Availability</h1>
-      <h1>Availability</h1>
-      <h1>Availability</h1>
-      <h1>Availability</h1>
+      <h1 className="txt-center">Selected Facility is: {facilityName}</h1>
+      <div>
+        <h1>Select Any of the Available Slot</h1>
+        {console.log(slotAvailibilty[0].slots)}
+        {slotAvailibilty[0].slots.map((slot, index) => {
+          return (
+            <div key={index}>
+              <div>Timing : {slot?.time}</div>
+              <div>Price Per Hour : {slot?.price}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Facilities.js
+++ src/components/Facilities.js
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 
 function Facilities() {
   return (
-    <div className="facility-container">
-      {facilitiesAvailable.map((facility) => {
-        return (
-          <Link to={`facility/${facility.name}`}>
-            <div className="facility-name">{facility.name}</div>
-          </Link>
-        );
-      })}
+    <div>
+      <h1 className="txt-center" style={{ color: "maroon" }}>
+        Select any Facilility to Book
+      </h1>
+      <div className="facility-container">
+        {facilitiesAvailable.map((facility, index) => {
+          return (
+            <Link key={index} to={`facility/${facility.name}`}>
+              <div className="facility-name">{facility.name}</div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
